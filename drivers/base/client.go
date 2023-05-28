c6a2a006d725c7f8f1101316992b5bdb9f187c27
qilme <89187439+qilme@users.noreply.github.com>
2023-05-29 07:40:25 +0800

    Change DefaultTimeout to 180

--- drivers/base/client.go
+++ drivers/base/client.go
@@ -15,7 +15,7 @@ var (
 	HttpClient       *http.Client
 )
 var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
-var DefaultTimeout = time.Second * 30
+var DefaultTimeout = time.Second * 180
 
 func InitClient() {
 	NoRedirectClient = resty.New().SetRedirectPolicy(
